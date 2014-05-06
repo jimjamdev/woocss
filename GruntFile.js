c7f0c418c05d22e48d2a64cee48e85a4e77dadcd
Ian Jamieson <cogocreative@gmail.com>
2014-05-06 13:19:59 +0200

    Updates

---
+++ GruntFile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+
+        // == Task List ==============================================
+
+
+
+        compass: {
+            build: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        },
+
+        // Merge Javascript Files
+        concat: {
+            js: {
+                src: [
+                    'vendor/components/jquery/jquery.js',
+                ],
+                dest: 'js/app.js'
+            }
+        },
+
+        // Compress Javascript
+        uglify: {
+            build: {
+                files: {
+                    'js/app.min.js': ['js/app.js']
+                }
+            }
+        },
+
+
+
+        // == Watch List =============================================
+
+        watch: {
+            files: ['GruntFile.js', 'html/*', 'css/*', 'img/*', 'js/*', 'img/*'],
+            options: {
+                livereload: true,
+                spawn: false,
+            },
+            sass: {
+                files: ['GruntFile.js', '**/*.scss', 'core/**/*', 'core/components/**/*'],
+                tasks: ['compass', 'cssmin']
+            },
+            js: {
+                files: ['GruntFile.js', 'core/components/**/*.js'],
+                tasks: ['concat', 'uglify']
+            },
+
+        }
+
+    });
+
+    grunt.registerTask('default', 'watch');
+
+}
