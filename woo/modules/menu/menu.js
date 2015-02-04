beda6d52bad3ba394366a9adcdac9912d62746c1
Ian Jamieson <cogocreative@gmail.com>
2015-02-04 15:26:21 +0100

    integrated https://github.com/paranoida/sass-mediaqueries

--- woo/modules/menu/menu.js
+++ woo/modules/menu/menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     var menuToggle = $('.menu').unbind();
-    $('.menu ul').removeClass("show");
+    $('[data-type="menu"] ul').removeClass("show");
 
     menuToggle.on('click', function (e) {
         e.preventDefault();
-        $('.menu ul').slideToggle(function () {
-            if ($('.menu ul').is(':hidden')) {
-                $('.menu ul').removeAttr('style');
+        $('[data-type="menu"] ul').slideToggle(function () {
+            if ($('[data-type="menu"] ul').is(':hidden')) {
+                $('[data-type="menu"] ul').removeAttr('style');
             }
         });
     });
