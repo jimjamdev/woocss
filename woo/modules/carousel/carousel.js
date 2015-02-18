e86ae5273c067c7b8389d53da8c1f8b9f67567ba
Ian Jamieson <cogocreative@gmail.com>
2015-02-18 11:12:04 +0100

    carousel

---
+++ woo/modules/carousel/carousel.js
@@ -0,0 +1,34 @@
+function($) {
+    $('.carousel').each(function() {
+        var $ele = $(this),
+            itemCount = $ele.data('items'),
+            useNav = $ele.data('nav') !== undefined ? $ele.data('nav') : true,
+            useAutoWidth = $ele.data('auto-width') !== undefined ? $ele.data('auto-width') : true,
+            margin = $ele.data('margin') !== undefined ? $ele.data('margin') : null,
+            options = {
+                dots: useNav,
+                nav: useNav,
+                autoWidth: useAutoWidth
+            };
+
+        if (margin !== null) {
+            options.margin = margin;
+        }
+
+        if (itemCount !== undefined) {
+            options.items = $.isArray(itemCount) ? itemCount[4] : itemCount
+            options.responsive = {
+                480:{
+                    items: $.isArray(itemCount) ? itemCount[0] : 2
+                },
+                678:{
+                    items: $.isArray(itemCount) ? itemCount[1] : 4
+                },
+                960:{
+                    items: $.isArray(itemCount) ? itemCount[3] : itemCount
+                }
+            };
+        };
+
+        $ele.owlCarousel(options);
+    });
