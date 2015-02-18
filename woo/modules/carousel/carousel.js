f89cc08e7ab1cce99875cf74f021fa9adfe5027d
Ian Jamieson <cogocreative@gmail.com>
2015-02-18 11:40:18 +0100

    fix for carousel.js

--- woo/modules/carousel/carousel.js
+++ woo/modules/carousel/carousel.js
@@ -1,34 +1,39 @@
-function($) {
-    $('.carousel').each(function() {
-        var $ele = $(this),
-            itemCount = $ele.data('items'),
-            useNav = $ele.data('nav') !== undefined ? $ele.data('nav') : true,
-            useAutoWidth = $ele.data('auto-width') !== undefined ? $ele.data('auto-width') : true,
-            margin = $ele.data('margin') !== undefined ? $ele.data('margin') : null,
-            options = {
-                dots: useNav,
-                nav: useNav,
-                autoWidth: useAutoWidth
-            };
+(function() {
 
-        if (margin !== null) {
-            options.margin = margin;
-        }
+   'use strict';
 
-        if (itemCount !== undefined) {
-            options.items = $.isArray(itemCount) ? itemCount[4] : itemCount
-            options.responsive = {
-                480:{
-                    items: $.isArray(itemCount) ? itemCount[0] : 2
-                },
-                678:{
-                    items: $.isArray(itemCount) ? itemCount[1] : 4
-                },
-                960:{
-                    items: $.isArray(itemCount) ? itemCount[3] : itemCount
-                }
-            };
-        };
+   $('.owl-carousel').each(function() {
+       var $ele = $(this),
+           itemCount = $ele.data('items'),
+           useNav = $ele.data('nav') !== undefined ? $ele.data('nav') : true,
+           useAutoWidth = $ele.data('auto-width') !== undefined ? $ele.data('auto-width') : true,
+           margin = $ele.data('margin') !== undefined ? $ele.data('margin') : null,
+           options = {
+               dots: useNav,
+               nav: useNav,
+               autoWidth: useAutoWidth
+           };
 
-        $ele.owlCarousel(options);
-    });
+       if (margin !== null) {
+           options.margin = margin;
+       }
+
+       if (itemCount !== undefined) {
+           options.items = $.isArray(itemCount) ? itemCount[4] : itemCount
+           options.responsive = {
+               480:{
+                   items: $.isArray(itemCount) ? itemCount[0] : 2
+               },
+               678:{
+                   items: $.isArray(itemCount) ? itemCount[1] : 4
+               },
+               960:{
+                   items: $.isArray(itemCount) ? itemCount[3] : itemCount
+               }
+           };
+       };
+
+       $ele.owlCarousel(options);
+   });
+
+    })();
